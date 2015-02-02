0915f2cfeef490f5200bc40fcb80f55ae4c261c5
Brian Weeteling <brianweeteling@gmail.com>
2015-02-02 16:31:32 +0100

    Add intro tour thingy (no content yet)

---
+++ ftu/js/app.js
@@ -0,0 +1,19 @@
+/* exported AppManager */
+'use strict';
+
+var AppManager = {
+
+  init: function init() {
+    this.isInitialized = true;
+
+    Tutorial.init();
+    Tutorial.start();
+
+    window.addEventListener('tutorialdone', tuturialDone);
+    function tuturialDone(){
+      window.location = 'typetest.html';
+    }
+  }
+};
+
+AppManager.init();
